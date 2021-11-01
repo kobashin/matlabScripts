707874c0da8d8bd6046f56b7e3bf6434692b5da4
genuine <shin5884genuine@outlook.com>
2021-11-02 00:54:04 +0900

    fix getCCallerFunctionName.m

--- getCCallerFunctionName.m
+++ getCCallerFunctionName.m
@@ -1,3 +1,7 @@
 load_system('ccaller');
 ccallerfunctions = cellfun(@(x)get_param(x, 'FunctionName'), find_system(gcs, 'BlockType', 'CCaller'), 'UniformOutput', false);
+out = sim('ccaller.slx', 'ReturnWorkspaceOutputs','on');
+save_system('ccaller')
+close_system('ccaller')
 disp(ccallerfunctions)
+disp(out.simoutData)
